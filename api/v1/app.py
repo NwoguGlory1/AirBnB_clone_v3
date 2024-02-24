73c614a5b5f60d30609bba58543dadf7fbbda9e5
Hovixen <orevaerijo@gmail.com>
2024-02-24 17:39:30 +0000

    task 5 error 404 handling without checks

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """ App controller module """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 from os import getenv
 from models import storage
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """ calls the storage.close functionality """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """ returns a Jsonify not found error """
+    return make_response(jsonify(error="Not found"), 404)
 
 
 if __name__ == '__main__':
