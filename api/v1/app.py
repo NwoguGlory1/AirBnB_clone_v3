ce62ee07abb438586d28426c93ed48dc3e913768
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-24 15:16:01 -0800

    Testing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-jsonify
 
 app = Flask(__name__)
 """ Creates an instance of class, Flask"""
 
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
