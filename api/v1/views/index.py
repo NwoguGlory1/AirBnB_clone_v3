da2fa7c9fb4e55302ffe41ac0b8b0fe720b2d403
Hovixen <orevaerijo@gmail.com>
2024-02-24 13:24:45 +0000

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
 """ module returns a JSON structure """
 from flask import jsonify
 from . import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ returns JSON status """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns the statistics of objects in JSON format """
+    objs = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+
+    objs_stat = {}
+
+    for obj in objs:
+        objs_stat[obj] = storage.count(obj)
+    return jsonify(**objs_stat)
