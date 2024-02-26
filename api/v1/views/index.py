8b193f63b257ca5ac06a5d43c82b31828e87e66e
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-26 10:07:59 -0800

    Tasks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
+""" Script for the index of the API""" 
 
-""" module returns a JSON structure """
 from flask import jsonify
-from . import app_views
+from models import storage
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ returns JSON status """
-    return jsonify(status="OK")
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """
+    Function that executes if user accesses '/status'
+    """
+    return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """ Function that executes if user accesses '/stats'"""
+    objects = {
+            'amenities': Amenity,
+            'cities': City,
+            'places': Place,
+            'reviews': Review,
+            'states': State,
+            'users': User
+            }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
