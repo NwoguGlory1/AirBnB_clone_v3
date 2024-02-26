da3103dc2482b0257c26d4f925753db5592d2502
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-26 11:03:27 -0800

    Tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """ App controller module """
+
 from flask import Flask
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 """ registers the blueprint to app """
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """ calls the storage.close functionality """
+    storage.close()
+
 @app.errorhandler(404)
 def not_found(error):
     """
@@ -21,12 +26,6 @@ def not_found(error):
     "error": "Not found" into a JSON response
     """
     return jsonify({"error": "Not found"}), 404
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """ calls the storage.close functionality """
-    storage.close()
 
 
 if __name__ == '__main__':
