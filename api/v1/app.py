a97c05d1278f5e214868ad5636424e86d8c0954b
Hovixen <orevaerijo@gmail.com>
2024-02-27 04:19:35 +0000

    app.py updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 """ App controller module """
 from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
+from flask_cors imort CORS
 from os import getenv
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CROS(app, resources={r"/*": {"origins": "0.0.0.0"}}
 
 
 @app.teardown_appcontext
